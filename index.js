52b10038ea6a758c84516b0a4a6459d5d4166cbd
Oleksandr Pereiaslov <oleksandrpereiaslov@MacBook-Pro-Oleksandr.local>
2022-02-22 19:11:59 +0200

    commit message

--- index.js
+++ index.js
@@ -4,21 +4,54 @@ const token = "5202216584:AAGieEKogHFyPAaSDLBRPFSOUi6pHTRhjR0";
 
 const bot = new TelegramApi(token, { polling: true });
 
-bot.on("message", (msg) => {
-  const text = msg.text;
-  const chatId = msg.chat.id;
-  // bot.sendMessage(chatId, `Ты мне написал (${text}) Это так мило`);
-  if (text === "/start") {
-    bot.sendMessage(
-      chatId,
-      `Добро пожаловать в телеграм бот по изучению Анг.Мовы Tovi`
-    );
-  }
-  if (text === "/info") {
-    bot.sendMessage(
-      chatId,
-      `тебя зовут  ${msg.from.first_name} ${msg.from.last_name}`
-    );
-  }
-  console.log(msg);
-});
+const start = () => {
+	// команды бота
+	bot.setMyCommands([
+		{ command: "/start", description: "/start" },
+		{ command: "/info", description: "/info" },
+		{ command: "/keyboard", description: "/keyboard" },
+	]);
+
+	bot.on("message", async (msg) => {
+		const text = msg.text;
+		const chatId = msg.chat.id;
+
+		// keyboard
+
+		if (text === "/start") {
+			await bot.sendMessage(chatId, "/start", {
+				reply_markup: {
+					keyboard: [["Описание"], ["Челенджы"]],
+				},
+			});
+		}
+
+		// bot.sendMessage(chatId, `Ты мне написал (${text}) Это так мило`);
+
+		// Старт бота
+		if (text === "/start") {
+			// стикер приветствия
+			await bot.sendSticker(
+				chatId,
+				`CAACAgIAAxkBAAED5Z9iBtbExYdZqmOOZkN3w9eMjDrDVAACbBoAAp_9OUjO9zrazER_2iME`
+			);
+			// сообщения приветствия
+			return bot.sendMessage(
+				chatId,
+				`Добро пожаловать в телеграм бот по изучению Анг.Мовы Tovi`
+			);
+			// предложения о выборе языка
+		}
+		// информацыя про пользавателя
+		if (text === "/info") {
+			return bot.sendMessage(
+				chatId,
+				`тебя зовут  ${msg.from.first_name} ${msg.from.last_name}`
+			);
+		}
+		return bot.sendMessage(chatId, "Я тебя не понимаю,попробуй еще раз! ");
+
+		console.log(msg);
+	});
+};
+start();
